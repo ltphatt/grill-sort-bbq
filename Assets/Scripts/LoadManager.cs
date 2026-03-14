2d7e50e38891afce52398c31439e00334ea152d8
Lê Thanh Phát <98292493+ltphatt@users.noreply.github.com>
2026-03-14 10:13:32 +0700

    add loading screen

---
+++ Assets/Scripts/LoadManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadManager : MonoBehaviour
+{
+    [SerializeField] Slider loadingBar;
+    [SerializeField] TextMeshProUGUI loadingText;
+
+    [SerializeField] float delay = 3f;
+
+    void Start()
+    {
+        StartCoroutine(LoadScene());
+    }
+
+    // Pseudo loading screen
+    IEnumerator LoadScene()
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync("Main");
+
+        operation.allowSceneActivation = false;
+
+        float elapsedTime = 0f;
+        while (elapsedTime < delay || operation.progress < .9f)
+        {
+            elapsedTime += Time.deltaTime;
+
+            float progress = Mathf.Clamp01(elapsedTime / delay);
+
+            loadingBar.value = progress;
+            loadingText.text = $"{Mathf.RoundToInt(progress * 100)}%";
+
+            yield return null;
+        }
+
+        loadingBar.value = 1f;
+        loadingText.text = "100%";
+
+        yield return new WaitForSeconds(0.5f);
+
+        operation.allowSceneActivation = true;
+    }
+}
